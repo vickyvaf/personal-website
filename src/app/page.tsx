dc6797811e25350e9d560445312616ba48d747ca
Vicky Adi Firmansyah <vickyadi243@gmail.com>
2025-09-10 23:38:08 +0700

    chore: remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 
